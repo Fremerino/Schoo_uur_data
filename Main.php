2f16fcbed8468a4ffc81e10de308354c2473e381
BuildTools <unconfigured@null.spigotmc.org>
2025-05-24 17:15:08 +0200

    estab connection

--- Main.php
+++ Main.php
@@ -3,10 +3,10 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "uur";
+$servername = "bsdsghpsfjuwwm24cxeo-mysql.services.clever-cloud.com";
+$username = "usj85unhcp8r8uqi";
+$password = "Kf3AxEqyt2bsNQQvl0Mi";
+$dbname = "bsdsghpsfjuwwm24cxeo";
 
 $Data_response = array();
 // Create connection
